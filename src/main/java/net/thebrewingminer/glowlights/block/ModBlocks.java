775ffa3d628d7b6da71e875437189b101cc8b345
TheBrewingMiner <asv2005@icloud.com>
2024-05-17 23:56:28 -0700

    Started creating a Glow Torch class. It can be placed in the world (floors only) and behaves properly with water and light. Nothing else such as translation key nor state and texture files yet.

--- src/main/java/net/thebrewingminer/glowlights/block/ModBlocks.java
+++ src/main/java/net/thebrewingminer/glowlights/block/ModBlocks.java
@@ -1,9 +1,11 @@
 package net.thebrewingminer.glowlights.block;
 
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -12,6 +14,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.thebrewingminer.glowlights.GlowLights;
 import net.thebrewingminer.glowlights.block.custom.GlowLanternBlock;
+import net.thebrewingminer.glowlights.block.custom.GlowTorchBlock;
 import net.thebrewingminer.glowlights.items.ModItems;
 
 import java.util.function.Supplier;
@@ -21,6 +24,9 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> GLOW_LANTERN_BLOCK = registerBlock("glow_lantern_block",
             () -> new GlowLanternBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(6f).lightLevel(state -> state.getValue(GlowLanternBlock.WATERLOGGED) ? 15 : 10)), CreativeModeTab.TAB_BUILDING_BLOCKS);
+
+    public static final RegistryObject<Block> GLOW_TORCH_BLOCK = registerBlock("glow_torch",
+            () ->new GlowTorchBlock(BlockBehaviour.Properties.of(Material.DECORATION).noCollission().instabreak().lightLevel(state -> state.getValue(GlowTorchBlock.WATERLOGGED) ? 15 : 10).sound(SoundType.WOOD), ParticleTypes.FLAME), CreativeModeTab.TAB_BUILDING_BLOCKS);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
---
+++ src/main/java/net/thebrewingminer/glowlights/block/custom/GlowTorchBlock.java
@@ -0,0 +1,86 @@
+package net.thebrewingminer.glowlights.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class GlowTorchBlock extends Block implements SimpleWaterloggedBlock {
+    protected static final int AABB_STANDING_OFFSET = 2;
+    protected static final VoxelShape AABB = Block.box(6.0, 0.0, 6.0, 10.0, 10.0, 10.0);
+    protected final ParticleOptions flameParticle;
+    public static final BooleanProperty WATERLOGGED;
+
+    static {
+        WATERLOGGED = BlockStateProperties.WATERLOGGED;
+    }
+
+    public GlowTorchBlock(BlockBehaviour.Properties properties, ParticleOptions particle) {
+        super(properties);
+        this.flameParticle = particle;
+        this.registerDefaultState((BlockState)this.stateDefinition.any().setValue(WATERLOGGED, false));
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+        return AABB;
+    }
+
+    @Override
+    public BlockState updateShape(BlockState state1, Direction direction, BlockState state2, LevelAccessor accessor, BlockPos pos1, BlockPos pos2) {
+        return direction == Direction.DOWN && !this.canSurvive(state1, accessor, pos1) ? Blocks.AIR.defaultBlockState() : super.updateShape(state1, direction, state2, accessor, pos1, pos2);
+    }
+
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader reader, BlockPos pos) {
+        return canSupportCenter(reader, pos.below(), Direction.UP);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return (Boolean) state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos blockPos, Player player, InteractionHand hand, BlockHitResult result){
+        if(!level.isClientSide() && hand == InteractionHand.MAIN_HAND){
+            level.setBlock(blockPos, state, 3);
+        }
+        return super.use(state, level, blockPos, player, hand, result);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder){
+        builder.add(WATERLOGGED);
+    }
+
+    @Override
+    public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource source) {
+        double $$4 = (double)pos.getX() + 0.5;
+        double $$5 = (double)pos.getY() + 0.7;
+        double $$6 = (double)pos.getZ() + 0.5;
+        level.addParticle(ParticleTypes.SMOKE, $$4, $$5, $$6, 0.0, 0.0, 0.0);
+        level.addParticle(this.flameParticle, $$4, $$5, $$6, 0.0, 0.0, 0.0);
+    }
+}
