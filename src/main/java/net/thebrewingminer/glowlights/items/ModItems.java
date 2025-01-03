0f79311ab5df1ef83d8ac76700f4397ca0c0b109
TheBrewingMiner <asv2005@icloud.com>
2025-01-03 00:47:41 -0800

    Filled out the prismarine family of glow campfires. Added [temporary] animated ash textures.

--- src/main/java/net/thebrewingminer/glowlights/items/ModItems.java
+++ src/main/java/net/thebrewingminer/glowlights/items/ModItems.java
@@ -25,6 +25,14 @@ public class ModItems {
             () -> new ItemNameBlockItem(ModBlocks.PRISMARINE_GLOW_CAMPFIRE.get(),
             new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
 
+    public static final RegistryObject<Item> PRISMARINE_BRICK_GLOW_CAMPFIRE = ITEMS.register("prismarine_brick_glow_campfire",
+            () -> new ItemNameBlockItem(ModBlocks.PRISMARINE_BRICK_GLOW_CAMPFIRE.get(),
+                    new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
+
+    public static final RegistryObject<Item> DARK_PRISMARINE_GLOW_CAMPFIRE = ITEMS.register("dark_prismarine_glow_campfire",
+            () -> new ItemNameBlockItem(ModBlocks.DARK_PRISMARINE_GLOW_CAMPFIRE.get(),
+                    new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
+
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }
