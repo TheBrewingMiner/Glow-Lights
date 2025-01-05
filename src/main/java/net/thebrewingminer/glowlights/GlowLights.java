818cd425241c87325f0815fb32ad8bba6d9e733c
TheBrewingMiner <asv2005@icloud.com>
2025-01-05 00:32:47 -0800

    Removed some comments.

--- src/main/java/net/thebrewingminer/glowlights/GlowLights.java
+++ src/main/java/net/thebrewingminer/glowlights/GlowLights.java
@@ -13,15 +13,11 @@ import net.thebrewingminer.glowlights.block.ModBlocks;
 import net.thebrewingminer.glowlights.items.ModItems;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(GlowLights.MODID)
 public class GlowLights {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "glowlights";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    // Create a Deferred Register to hold Blocks which will all be registered under the "glowlights" namespace
     public GlowLights() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
@@ -37,7 +33,6 @@ public class GlowLights {
 
     private void commonSetup(final FMLCommonSetupEvent event) {}
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
