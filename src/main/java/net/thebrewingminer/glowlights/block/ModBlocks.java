88d8b2890ca34bdfaf2b703a65c6ae60004a9404
TheBrewingMiner <asv2005@icloud.com>
2025-01-08 17:07:39 -0800

    Changed .copy operator to .ofFullCopy

--- src/main/java/net/thebrewingminer/glowlights/block/ModBlocks.java
+++ src/main/java/net/thebrewingminer/glowlights/block/ModBlocks.java
@@ -23,33 +23,33 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, GlowLights.MODID);
 
     public static final RegistryObject<Block> GLOW_LANTERN_BLOCK = registerBlock("glow_lantern_block",
-            () -> new GlowLanternBlock(BlockBehaviour.Properties.copy(Blocks.SEA_LANTERN).lightLevel(GlowLanternBlock::getLightLevel))
+            () -> new GlowLanternBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.SEA_LANTERN).lightLevel(GlowLanternBlock::getLightLevel))
     );
 
     public static final RegistryObject<Block> GLOW_TORCH_BLOCK = BLOCKS.register("glow_torch",
-            () -> new GlowTorchBlock(BlockBehaviour.Properties.copy(Blocks.TORCH).lightLevel(GlowTorchBlock::getLightLevel), ParticleTypes.GLOW)
+            () -> new GlowTorchBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.TORCH).lightLevel(GlowTorchBlock::getLightLevel), ParticleTypes.GLOW)
     );
 
     public static final RegistryObject<Block> GLOW_WALL_TORCH_BLOCK = BLOCKS.register("glow_wall_torch",
-            () -> new GlowWallTorchBlock(BlockBehaviour.Properties.copy(Blocks.WALL_TORCH).lightLevel(GlowWallTorchBlock::getLightLevel), ParticleTypes.GLOW)
+            () -> new GlowWallTorchBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.WALL_TORCH).lightLevel(GlowWallTorchBlock::getLightLevel), ParticleTypes.GLOW)
     );
 
     public static final RegistryObject<Block> PRISMARINE_GLOW_CAMPFIRE = BLOCKS.register("prismarine_glow_campfire",
-            () -> new GlowCampfireBlock(BlockBehaviour.Properties.copy(Blocks.PRISMARINE)
+            () -> new GlowCampfireBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.PRISMARINE)
                     .lightLevel(GlowCampfireBlock::getLightLevel)
                     .noOcclusion()
             )
     );
 
     public static final RegistryObject<Block> PRISMARINE_BRICK_GLOW_CAMPFIRE = BLOCKS.register("prismarine_brick_glow_campfire",
-            () -> new GlowCampfireBlock(BlockBehaviour.Properties.copy(Blocks.PRISMARINE_BRICKS)
+            () -> new GlowCampfireBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.PRISMARINE_BRICKS)
                     .lightLevel(GlowCampfireBlock::getLightLevel)
                     .noOcclusion()
             )
     );
 
     public static final RegistryObject<Block> DARK_PRISMARINE_GLOW_CAMPFIRE = BLOCKS.register("dark_prismarine_glow_campfire",
-            () -> new GlowCampfireBlock(BlockBehaviour.Properties.copy(Blocks.DARK_PRISMARINE)
+            () -> new GlowCampfireBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.DARK_PRISMARINE)
                     .lightLevel(GlowCampfireBlock::getLightLevel)
                     .noOcclusion()
             )
