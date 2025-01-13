2d8a0ef742aecced1ff8860a7e25f6c641ad35c2
TheBrewingMiner <asv2005@icloud.com>
2025-01-12 18:20:16 -0800

    Cleaned up imports.

--- src/main/java/net/thebrewingminer/glowlights/block/ModBlocks.java
+++ src/main/java/net/thebrewingminer/glowlights/block/ModBlocks.java
@@ -6,8 +6,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
