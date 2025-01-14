0a88e85cc299502a8b700357170524e4e39a2b32
TheBrewingMiner <asv2005@icloud.com>
2025-01-13 16:20:33 -0800

    Added setID() property to each block.

--- src/main/java/net/thebrewingminer/glowlights/block/ModBlocks.java
+++ src/main/java/net/thebrewingminer/glowlights/block/ModBlocks.java
@@ -1,6 +1,9 @@
 package net.thebrewingminer.glowlights.block;
 
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -23,21 +26,26 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, GlowLights.MODID);
 
     public static final RegistryObject<Block> GLOW_LANTERN_BLOCK = registerBlock("glow_lantern_block",
-            () -> new GlowLanternBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.SEA_LANTERN).lightLevel(GlowLanternBlock::getLightLevel))
+            () -> new GlowLanternBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.SEA_LANTERN).lightLevel(GlowLanternBlock::getLightLevel)
+                    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(GlowLights.MODID, "glow_lantern_block")))
+            )
     );
 
     public static final RegistryObject<Block> GLOW_TORCH_BLOCK = BLOCKS.register("glow_torch",
-            () -> new GlowTorchBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.TORCH).lightLevel(GlowTorchBlock::getLightLevel), ParticleTypes.GLOW)
+            () -> new GlowTorchBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.TORCH).lightLevel(GlowTorchBlock::getLightLevel)
+                    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(GlowLights.MODID, "glow_torch"))), ParticleTypes.GLOW)
     );
 
     public static final RegistryObject<Block> GLOW_WALL_TORCH_BLOCK = BLOCKS.register("glow_wall_torch",
-            () -> new GlowWallTorchBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.TORCH).lightLevel(GlowWallTorchBlock::getLightLevel), ParticleTypes.GLOW)
+            () -> new GlowWallTorchBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.TORCH).lightLevel(GlowWallTorchBlock::getLightLevel)
+                    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(GlowLights.MODID, "glow_wall_torch"))), ParticleTypes.GLOW)
     );
 
     public static final RegistryObject<Block> PRISMARINE_GLOW_CAMPFIRE = BLOCKS.register("prismarine_glow_campfire",
             () -> new GlowCampfireBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.PRISMARINE)
                     .lightLevel(GlowCampfireBlock::getLightLevel)
                     .noOcclusion()
+                    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(GlowLights.MODID, "prismarine_glow_campfire")))
             )
     );
 
@@ -45,6 +53,7 @@ public class ModBlocks {
             () -> new GlowCampfireBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.PRISMARINE_BRICKS)
                     .lightLevel(GlowCampfireBlock::getLightLevel)
                     .noOcclusion()
+                    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(GlowLights.MODID, "prismarine_brick_glow_campfire")))
             )
     );
 
@@ -52,6 +61,7 @@ public class ModBlocks {
             () -> new GlowCampfireBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.DARK_PRISMARINE)
                     .lightLevel(GlowCampfireBlock::getLightLevel)
                     .noOcclusion()
+                    .setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(GlowLights.MODID, "dark_pirsmarine_glow_campfire")))
             )
     );
 
