35e89df1ca752fd5175fce867d668e36b6e0b97f
TheBrewingMiner <asv2005@icloud.com>
2025-01-13 23:47:46 -0800

    Added .setId() to each item.

--- src/main/java/net/thebrewingminer/glowlights/items/ModItems.java
+++ src/main/java/net/thebrewingminer/glowlights/items/ModItems.java
@@ -1,6 +1,9 @@
 package net.thebrewingminer.glowlights.items;
 
 import net.minecraft.core.Direction;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,24 +19,25 @@ public class ModItems {
             () -> new StandingAndWallBlockItem(
                     ModBlocks.GLOW_TORCH_BLOCK.get(),
                     ModBlocks.GLOW_WALL_TORCH_BLOCK.get(),
-                    Direction.DOWN, new Item.Properties())
+                    Direction.DOWN, new Item.Properties().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(GlowLights.MODID, "glow_torch")))
+            )
     );
 
     public static final RegistryObject<Item> PRISMARINE_GLOW_CAMPFIRE = ITEMS.register("prismarine_glow_campfire",
             () -> new BlockItem(ModBlocks.PRISMARINE_GLOW_CAMPFIRE.get(),
-                new Item.Properties()
+                new Item.Properties().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(GlowLights.MODID, "prismarine_glow_campfire")))
             )
     );
 
     public static final RegistryObject<Item> PRISMARINE_BRICK_GLOW_CAMPFIRE = ITEMS.register("prismarine_brick_glow_campfire",
             () -> new BlockItem(ModBlocks.PRISMARINE_BRICK_GLOW_CAMPFIRE.get(),
-                    new Item.Properties()
+                    new Item.Properties().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(GlowLights.MODID, "prismarine_brick_glow_campfire")))
             )
     );
 
     public static final RegistryObject<Item> DARK_PRISMARINE_GLOW_CAMPFIRE = ITEMS.register("dark_prismarine_glow_campfire",
             () -> new BlockItem(ModBlocks.DARK_PRISMARINE_GLOW_CAMPFIRE.get(),
-                    new Item.Properties()
+                    new Item.Properties().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(GlowLights.MODID, "dark_prismarine_glow_campfire")))
             )
     );
 
