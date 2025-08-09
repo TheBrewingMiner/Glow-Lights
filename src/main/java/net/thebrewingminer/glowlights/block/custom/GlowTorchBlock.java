4ff8e31c9a9cefa1275370e65f5249c79d4eb3ed
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-08-09 02:09:56 -0700

    Cleaned up redundant overrides, abstracted particle additions, and fixed water not updating when water logging a torch.

--- src/main/java/net/thebrewingminer/glowlights/block/custom/GlowTorchBlock.java
+++ src/main/java/net/thebrewingminer/glowlights/block/custom/GlowTorchBlock.java
@@ -5,14 +5,10 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
@@ -24,16 +20,19 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.level.material.PushReaction;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
+@SuppressWarnings({"NullableProblems", "deprecation"})
 public class GlowTorchBlock extends TorchBlock implements SimpleWaterloggedBlock {
-    protected static final int AABB_STANDING_OFFSET = 2;
     protected static final VoxelShape AABB = Block.box(6.0, 0.0, 6.0, 10.0, 10.0, 10.0);
     protected final ParticleOptions flameParticle;
     public static final BooleanProperty WATERLOGGED;
+
+    public static final int WATERLOGGED_SMOKE_DELAY = 7;
+    public static final int WATERLOGGED_PARTICLE_DELAY = 3;
+    public static final int DRY_SMOKE_DELAY = 2;
+    public static final int DRY_PARTICLE_DELAY = 7;
 
     static {
         WATERLOGGED = BlockStateProperties.WATERLOGGED;
@@ -46,79 +45,63 @@ public class GlowTorchBlock extends TorchBlock implements SimpleWaterloggedBlock
     }
 
     @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder){
+        builder.add(WATERLOGGED);
+    }
+
+    @Override
     public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
         return AABB;
     }
 
-    public BlockState getStateForPlacement(BlockPlaceContext context) {
-        LevelAccessor levelaccessor = context.getLevel();
-        BlockPos blockpos = context.getClickedPos();
-        boolean flag = levelaccessor.getFluidState(blockpos).getType() == Fluids.WATER;
-        return this.defaultBlockState().setValue(WATERLOGGED, flag);
-    }
-    
     @Override
-    public BlockState updateShape(BlockState state1, Direction direction, BlockState state2, LevelAccessor accessor, BlockPos pos1, BlockPos pos2) {
-        return direction == Direction.DOWN && !this.canSurvive(state1, accessor, pos1) ? Blocks.AIR.defaultBlockState() : super.updateShape(state1, direction, state2, accessor, pos1, pos2);
+    public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
+        if (pState.getValue(WATERLOGGED)) {
+            pLevel.scheduleTick(pCurrentPos, Fluids.WATER, Fluids.WATER.getTickDelay(pLevel));
+
+        }
+        return pFacing == Direction.DOWN && !this.canSurvive(pState, pLevel, pCurrentPos) ? Blocks.AIR.defaultBlockState() : super.updateShape(pState, pFacing, pFacingState, pLevel, pCurrentPos, pFacingPos);
     }
 
     @Override
-    public boolean canSurvive(BlockState state, LevelReader reader, BlockPos pos) {
-        return canSupportCenter(reader, pos.below(), Direction.UP);
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        LevelAccessor levelAccessor = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        boolean inWater = levelAccessor.getFluidState(pos).getType() == Fluids.WATER;
+        return (this.defaultBlockState().setValue(WATERLOGGED, inWater));
     }
 
     @Override
     public FluidState getFluidState(BlockState state) {
         return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
     }
-
-    @Override
-    public InteractionResult use(BlockState state, Level level, BlockPos blockPos, Player player, InteractionHand hand, BlockHitResult result){
-        if(!level.isClientSide() && hand == InteractionHand.MAIN_HAND){
-            level.setBlock(blockPos, state, 3);
-        }
-        return super.use(state, level, blockPos, player, hand, result);
-    }
-
+    
     public static boolean isWaterlogged(BlockState state){
         return state.getValue(WATERLOGGED);
     }
 
     public static int getLightLevel(BlockState state){
-        int lightLevel = (isWaterlogged(state)) ? 15 : 10;
-        return lightLevel;
+        return (isWaterlogged(state)) ? 15 : 10;
     }
 
-    @Override
-    public PushReaction getPistonPushReaction(BlockState p_153494_) {
-        return PushReaction.DESTROY;
+    public static void addFlameParticle(ParticleOptions particle, Level level, BlockPos pos, RandomSource randomSource, int integer){
+        double x = (double)pos.getX() + 0.5;
+        double y = (double)pos.getY() + 0.7;
+        double z = (double)pos.getZ() + 0.5;
+
+        if (randomSource.nextInt(integer) == 0) {
+            level.addParticle(particle, x, y, z, 0.0, 0.0, 0.0);
+        }
     }
-
+    
     @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder){
-        builder.add(WATERLOGGED);
-    }
-
-    @Override
-    public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource source) {
-        double $$4 = (double)pos.getX() + 0.5;
-        double $$5 = (double)pos.getY() + 0.7;
-        double $$6 = (double)pos.getZ() + 0.5;
-
+    public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource randomSource) {
         if (state.getValue(WATERLOGGED)) {
-            if (source.nextInt(7) == 0){
-                level.addParticle(ParticleTypes.SMOKE, $$4, $$5, $$6, 0.0, 0.0, 0.0);
-            }
-            if(source.nextInt(3) == 0){
-                level.addParticle(this.flameParticle, $$4, $$5, $$6, 0.0, 0.0, 0.0);
-            }
+            addFlameParticle(ParticleTypes.SMOKE, level, pos, randomSource, WATERLOGGED_SMOKE_DELAY);
+            addFlameParticle(this.flameParticle, level, pos, randomSource, WATERLOGGED_PARTICLE_DELAY);
         } else {
-            if (source.nextInt(2) == 0){
-                level.addParticle(ParticleTypes.SMOKE, $$4, $$5, $$6, 0.0, 0.0, 0.0);
-            }
-            if (source.nextInt(7) == 0) {
-                level.addParticle(this.flameParticle, $$4, $$5, $$6, 0.0, 0.0, 0.0);
-            }
+            addFlameParticle(ParticleTypes.SMOKE, level, pos, randomSource, DRY_SMOKE_DELAY);
+            addFlameParticle(this.flameParticle, level, pos, randomSource,DRY_PARTICLE_DELAY);
         }
     }
 }
