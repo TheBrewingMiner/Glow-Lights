b58b5d3312a97873511c704bb9351cb9b6ebf46c
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-08-09 17:53:04 -0700

    Added Glow Lantern. Formatting.

--- src/main/java/net/thebrewingminer/glowlights/block/ModBlocks.java
+++ src/main/java/net/thebrewingminer/glowlights/block/ModBlocks.java
@@ -12,10 +12,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.thebrewingminer.glowlights.GlowLights;
-import net.thebrewingminer.glowlights.block.custom.GlowCampfireBlock;
-import net.thebrewingminer.glowlights.block.custom.GlowLanternBlock;
-import net.thebrewingminer.glowlights.block.custom.GlowTorchBlock;
-import net.thebrewingminer.glowlights.block.custom.GlowWallTorchBlock;
+import net.thebrewingminer.glowlights.block.custom.*;
 import net.thebrewingminer.glowlights.items.ModItems;
 
 import java.util.function.Supplier;
@@ -23,37 +20,9 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, GlowLights.MOD_ID);
 
-    public static final RegistryObject<Block> GLOW_LANTERN_BLOCK = registerBlock("glow_lantern_block",
-            () -> new GlowLanternBlock(BlockBehaviour.Properties.copy(Blocks.SEA_LANTERN).lightLevel(GlowLanternBlock::getLightLevel)), CreativeModeTab.TAB_BUILDING_BLOCKS);
-
-    public static final RegistryObject<Block> GLOW_TORCH_BLOCK = BLOCKS.register("glow_torch",
-            () -> new GlowTorchBlock(BlockBehaviour.Properties.copy(Blocks.TORCH).lightLevel(GlowTorchBlock::getLightLevel), ParticleTypes.GLOW)
-    );
-
-    public static final RegistryObject<Block> GLOW_WALL_TORCH_BLOCK = BLOCKS.register("glow_wall_torch",
-            () -> new GlowWallTorchBlock(BlockBehaviour.Properties.copy(Blocks.WALL_TORCH).lightLevel(GlowWallTorchBlock::getLightLevel), ParticleTypes.GLOW)
-    );
-
-    public static final RegistryObject<Block> PRISMARINE_GLOW_CAMPFIRE = BLOCKS.register("prismarine_glow_campfire",
-            () -> new GlowCampfireBlock(BlockBehaviour.Properties.copy(Blocks.PRISMARINE)
-                    .lightLevel(GlowCampfireBlock::getLightLevel)
-                    .noOcclusion()
-            )
-    );
-
-    public static final RegistryObject<Block> PRISMARINE_BRICK_GLOW_CAMPFIRE = BLOCKS.register("prismarine_brick_glow_campfire",
-            () -> new GlowCampfireBlock(BlockBehaviour.Properties.copy(Blocks.PRISMARINE_BRICKS)
-                    .lightLevel(GlowCampfireBlock::getLightLevel)
-                    .noOcclusion()
-            )
-    );
-
-    public static final RegistryObject<Block> DARK_PRISMARINE_GLOW_CAMPFIRE = BLOCKS.register("dark_prismarine_glow_campfire",
-            () -> new GlowCampfireBlock(BlockBehaviour.Properties.copy(Blocks.DARK_PRISMARINE)
-                    .lightLevel(GlowCampfireBlock::getLightLevel)
-                    .noOcclusion()
-            )
-    );
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab){
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
@@ -62,9 +31,52 @@ public class ModBlocks {
         return toReturn;
     }
 
-    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab){
-        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
-    }
+    /* Blocks */
+    public static final RegistryObject<Block> GLOW_LANTERN_BLOCK = registerBlock(
+        "glow_lantern_block",
+        () -> new GlowLanternBlock(BlockBehaviour.Properties.copy(Blocks.SEA_LANTERN).lightLevel(GlowLanternBlock::getLightLevel)),
+        CreativeModeTab.TAB_BUILDING_BLOCKS
+    );
+
+    public static final RegistryObject<Block> GLOW_LANTERN = registerBlock(
+        "glow_lantern",
+        () -> new GlowLantern(BlockBehaviour.Properties.copy(Blocks.LANTERN).lightLevel(GlowLantern::getLightLevel)),
+        CreativeModeTab.TAB_DECORATIONS
+    );
+
+    public static final RegistryObject<Block> GLOW_TORCH_BLOCK = BLOCKS.register(
+        "glow_torch",
+        () -> new GlowTorchBlock(BlockBehaviour.Properties.copy(Blocks.TORCH).lightLevel(GlowTorchBlock::getLightLevel), ParticleTypes.GLOW)
+    );
+
+    public static final RegistryObject<Block> GLOW_WALL_TORCH_BLOCK = BLOCKS.register(
+        "glow_wall_torch",
+        () -> new GlowWallTorchBlock(BlockBehaviour.Properties.copy(Blocks.WALL_TORCH).lightLevel(GlowWallTorchBlock::getLightLevel), ParticleTypes.GLOW)
+    );
+
+    public static final RegistryObject<Block> PRISMARINE_GLOW_CAMPFIRE = BLOCKS.register(
+        "prismarine_glow_campfire",
+        () -> new GlowCampfireBlock(BlockBehaviour.Properties.copy(Blocks.PRISMARINE)
+            .lightLevel(GlowCampfireBlock::getLightLevel)
+            .noOcclusion()
+        )
+    );
+
+    public static final RegistryObject<Block> PRISMARINE_BRICK_GLOW_CAMPFIRE = BLOCKS.register(
+        "prismarine_brick_glow_campfire",
+        () -> new GlowCampfireBlock(BlockBehaviour.Properties.copy(Blocks.PRISMARINE_BRICKS)
+            .lightLevel(GlowCampfireBlock::getLightLevel)
+            .noOcclusion()
+        )
+    );
+
+    public static final RegistryObject<Block> DARK_PRISMARINE_GLOW_CAMPFIRE = BLOCKS.register(
+        "dark_prismarine_glow_campfire",
+        () -> new GlowCampfireBlock(BlockBehaviour.Properties.copy(Blocks.DARK_PRISMARINE)
+            .lightLevel(GlowCampfireBlock::getLightLevel)
+            .noOcclusion()
+        )
+    );
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
---
+++ src/main/java/net/thebrewingminer/glowlights/block/custom/GlowLantern.java
@@ -0,0 +1,19 @@
+package net.thebrewingminer.glowlights.block.custom;
+
+import net.minecraft.world.level.block.LanternBlock;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class GlowLantern extends LanternBlock implements SimpleWaterloggedBlock {
+    public GlowLantern(Properties pProperties) {
+        super(pProperties);
+    }
+
+    public static boolean isWaterlogged(BlockState state){
+        return state.getValue(WATERLOGGED);
+    }
+
+    public static int getLightLevel(BlockState state){
+        return (isWaterlogged(state)) ? 15 : 10;
+    }
+}
