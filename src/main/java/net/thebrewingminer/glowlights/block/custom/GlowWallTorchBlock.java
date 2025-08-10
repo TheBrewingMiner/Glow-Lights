4e00a142ec3dc50354ce94d6ea8399ba3e0c0076
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-08-09 17:25:37 -0700

    Cleaned up redundant overrides, reorganized methods, and rewrote particle addition logic.

--- src/main/java/net/thebrewingminer/glowlights/block/custom/GlowWallTorchBlock.java
+++ src/main/java/net/thebrewingminer/glowlights/block/custom/GlowWallTorchBlock.java
@@ -1,18 +1,11 @@
 package net.thebrewingminer.glowlights.block.custom;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-import java.util.Map;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
@@ -23,19 +16,22 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.level.material.PushReaction;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class GlowWallTorchBlock extends WallTorchBlock implements SimpleWaterloggedBlock{
     public static final DirectionProperty FACING;
-    protected static final float AABB_OFFSET = 2.5F;
-    private static final Map<Direction, VoxelShape> AABBS;
     public static final BooleanProperty WATERLOGGED;
+
+    public static final int WATERLOGGED_SMOKE_DELAY = 7;
+    public static final int WATERLOGGED_PARTICLE_DELAY = 3;
+    public static final int DRY_SMOKE_DELAY = 2;
+    public static final int DRY_PARTICLE_DELAY = 7;
+
+    static {
+        FACING = HorizontalDirectionalBlock.FACING;
+        WATERLOGGED = BlockStateProperties.WATERLOGGED;
+    }
 
     public GlowWallTorchBlock(BlockBehaviour.Properties properties, ParticleOptions particle) {
         super(properties, particle);
@@ -43,47 +39,38 @@ public class GlowWallTorchBlock extends WallTorchBlock implements SimpleWaterlog
     }
 
     @Override
-    public String getDescriptionId() {
-        return this.asItem().getDescriptionId();
-    }
-
-    @Override
-    public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
-        return getShape(state);
-    }
-
-    public static VoxelShape getShape(BlockState state) {
-        return AABBS.get(state.getValue(FACING));
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING, WATERLOGGED);
     }
 
     @Override
     public boolean canSurvive(BlockState state, LevelReader reader, BlockPos pos) {
-        Direction $$3 = state.getValue(FACING);
-        BlockPos $$4 = pos.relative($$3.getOpposite());
-        BlockState $$5 = reader.getBlockState($$4);
-        return $$5.isFaceSturdy(reader, $$4, $$3);
+        Direction facindDirection = state.getValue(FACING);
+        BlockPos oppositePos = pos.relative(facindDirection.getOpposite());
+        BlockState blockState = reader.getBlockState(oppositePos);
+        return blockState.isFaceSturdy(reader, oppositePos, facindDirection);
     }
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        BlockState $$1 = this.defaultBlockState();
-        LevelReader $$2 = context.getLevel();
-        BlockPos $$3 = context.getClickedPos();
-        Direction[] $$4 = context.getNearestLookingDirections();
-        Direction[] var6 = $$4;
-        int var7 = $$4.length;
+        BlockState blockState = this.defaultBlockState();
+        LevelReader levelContext = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        Direction[] lookingDirections = context.getNearestLookingDirections();
+        Direction[] lookingDirections2 = lookingDirections;
+        int directionsLookingAt = lookingDirections.length;
 
         LevelAccessor levelaccessor = context.getLevel();
-        BlockPos blockpos = context.getClickedPos();
-        boolean flag = levelaccessor.getFluidState(blockpos).getType() == Fluids.WATER;
+        BlockPos clickedPos = context.getClickedPos();
+        boolean inWater = levelaccessor.getFluidState(clickedPos).getType() == Fluids.WATER;
 
-        for(int var8 = 0; var8 < var7; ++var8) {
-            Direction $$5 = var6[var8];
-            if ($$5.getAxis().isHorizontal()) {
-                Direction $$6 = $$5.getOpposite();
-                $$1 = $$1.setValue(FACING, $$6);
-                if ($$1.canSurvive($$2, $$3)) {
-                    return $$1.setValue(WATERLOGGED, flag);
+        for(int i = 0; i < directionsLookingAt; ++i) {
+            Direction direction = lookingDirections2[i];
+            if (direction.getAxis().isHorizontal()) {
+                Direction oppositeDirection = direction.getOpposite();
+                blockState = blockState.setValue(FACING, oppositeDirection);
+                if (blockState.canSurvive(levelContext, pos)) {
+                    return blockState.setValue(WATERLOGGED, inWater);
                 }
             }
         }
@@ -92,56 +79,12 @@ public class GlowWallTorchBlock extends WallTorchBlock implements SimpleWaterlog
     }
 
     @Override
-    public PushReaction getPistonPushReaction(BlockState p_153494_) {
-        return PushReaction.DESTROY;
-    }
+    public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
+        if (pState.getValue(WATERLOGGED)) {
+            pLevel.scheduleTick(pCurrentPos, Fluids.WATER, Fluids.WATER.getTickDelay(pLevel));
 
-    @Override
-    public BlockState updateShape(BlockState state, Direction direction, BlockState state2, LevelAccessor level, BlockPos pos, BlockPos pos2) {
-        return direction.getOpposite() == state.getValue(FACING) && !state.canSurvive(level, pos) ? Blocks.AIR.defaultBlockState() : state;
-    }
-
-    @Override
-    public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource source) {
-        Direction $$4 = state.getValue(FACING);
-        double $$5 = (double)pos.getX() + 0.5;
-        double $$6 = (double)pos.getY() + 0.7;
-        double $$7 = (double)pos.getZ() + 0.5;
-        double $$8 = 0.22;
-        double $$9 = 0.27;
-        Direction $$10 = $$4.getOpposite();
-
-        if (state.getValue(WATERLOGGED)){
-            if (source.nextInt(7) == 0){
-                level.addParticle(ParticleTypes.SMOKE, $$5 + 0.27 * (double)$$10.getStepX(), $$6 + 0.22, $$7 + 0.27 * (double)$$10.getStepZ(), 0.0, 0.0, 0.0);
-            }
-            if(source.nextInt(3) == 0){
-                level.addParticle(this.flameParticle, $$5 + 0.27 * (double)$$10.getStepX(), $$6 + 0.22, $$7 + 0.27 * (double)$$10.getStepZ(), 0.0, 0.0, 0.0);
-            }
-        } else {
-            if (source.nextInt(2) == 0){
-                level.addParticle(ParticleTypes.SMOKE, $$5 + 0.27 * (double)$$10.getStepX(), $$6 + 0.22, $$7 + 0.27 * (double)$$10.getStepZ(), 0.0, 0.0, 0.0);
-            }
-
-            if (source.nextInt(7) == 0){
-                level.addParticle(this.flameParticle, $$5 + 0.27 * (double)$$10.getStepX(), $$6 + 0.22, $$7 + 0.27 * (double)$$10.getStepZ(), 0.0, 0.0, 0.0);
-            }
         }
-    }
-
-    @Override
-    public BlockState rotate(BlockState state, Rotation rotation) {
-        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
-    }
-
-    @Override
-    public BlockState mirror(BlockState state, Mirror mirror) {
-        return state.rotate(mirror.getRotation(state.getValue(FACING)));
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(FACING, WATERLOGGED);
+        return pFacing == Direction.DOWN && !this.canSurvive(pState, pLevel, pCurrentPos) ? Blocks.AIR.defaultBlockState() : super.updateShape(pState, pFacing, pFacingState, pLevel, pCurrentPos, pFacingPos);
     }
 
     @Override
@@ -154,21 +97,40 @@ public class GlowWallTorchBlock extends WallTorchBlock implements SimpleWaterlog
     }
 
     public static int getLightLevel(BlockState state){
-        int lightLevel = (isWaterlogged(state)) ? 15 : 10;
-        return lightLevel;
+        return ((isWaterlogged(state)) ? 15 : 10);
+    }
+
+    public static void addFlameParticle(ParticleOptions particle, Level level, BlockState blockState, BlockPos pos, RandomSource randomSource, int delay){
+        Direction facingDirection = blockState.getValue(FACING);
+        Direction directionOpposite = facingDirection.getOpposite();
+        double centerX = (double)pos.getX() + 0.5;
+        double centerY = (double)pos.getY() + 0.7;
+        double centerZ = (double)pos.getZ() + 0.5;
+
+        double xzOffset = 0.27;
+        double yOffset = 0.22;
+
+        double xSpeed = 0.0;
+        double ySpeed = 0.0;
+        double zSpeed = 0.0;
+
+        double x = centerX + xzOffset * (double)directionOpposite.getStepX();
+        double y = centerY + yOffset;
+        double z = centerZ + xzOffset * (double)directionOpposite.getStepZ();
+
+        if (randomSource.nextInt(delay) == 0) {
+            level.addParticle(particle, x, y, z, xSpeed, ySpeed, zSpeed);
+        }
     }
 
     @Override
-    public InteractionResult use(BlockState state, Level level, BlockPos blockPos, Player player, InteractionHand hand, BlockHitResult result){
-        if(!level.isClientSide() && hand == InteractionHand.MAIN_HAND){
-            level.setBlock(blockPos, state, 3);
+    public void animateTick(BlockState blockState, Level level, BlockPos pos, RandomSource randomSource) {
+        if (blockState.getValue(WATERLOGGED)){
+            addFlameParticle(ParticleTypes.SMOKE, level, blockState, pos, randomSource, WATERLOGGED_SMOKE_DELAY);
+            addFlameParticle(this.flameParticle, level, blockState, pos, randomSource, WATERLOGGED_PARTICLE_DELAY);
+        } else {
+            addFlameParticle(ParticleTypes.SMOKE, level, blockState, pos, randomSource, DRY_SMOKE_DELAY);
+            addFlameParticle(this.flameParticle, level, blockState, pos, randomSource, DRY_PARTICLE_DELAY);
         }
-        return super.use(state, level, blockPos, player, hand, result);
-    }
-
-    static {
-        FACING = HorizontalDirectionalBlock.FACING;
-        AABBS = Maps.newEnumMap(ImmutableMap.of(Direction.NORTH, Block.box(5.5, 3.0, 11.0, 10.5, 13.0, 16.0), Direction.SOUTH, Block.box(5.5, 3.0, 0.0, 10.5, 13.0, 5.0), Direction.WEST, Block.box(11.0, 3.0, 5.5, 16.0, 13.0, 10.5), Direction.EAST, Block.box(0.0, 3.0, 5.5, 5.0, 13.0, 10.5)));
-        WATERLOGGED = BlockStateProperties.WATERLOGGED;
     }
 }
