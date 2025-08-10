971249fe1719d069a2329b95c1917f67b369e7b5
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-08-09 17:51:14 -0700

    Formatting

--- src/main/java/net/thebrewingminer/glowlights/items/ModItems.java
+++ src/main/java/net/thebrewingminer/glowlights/items/ModItems.java
@@ -11,29 +11,31 @@ import net.thebrewingminer.glowlights.block.ModBlocks;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GlowLights.MOD_ID);
 
-//  Item registered prior to my understanding of the code used within the ModBlocks class.
-
-    public static final RegistryObject<Item> GLOW_TORCH = ITEMS.register("glow_torch",
-            () -> new StandingAndWallBlockItem(
-                    ModBlocks.GLOW_TORCH_BLOCK.get(),
-                    ModBlocks.GLOW_WALL_TORCH_BLOCK.get(),
-                    new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)
-            )
+    /* Items */
+    public static final RegistryObject<Item> GLOW_TORCH = ITEMS.register(
+        "glow_torch",
+        () -> new StandingAndWallBlockItem(
+            ModBlocks.GLOW_TORCH_BLOCK.get(),
+            ModBlocks.GLOW_WALL_TORCH_BLOCK.get(),
+            new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)
+        )
     );
 
-    public static final RegistryObject<Item> PRISMARINE_GLOW_CAMPFIRE = ITEMS.register("prismarine_glow_campfire",
-            () -> new ItemNameBlockItem(ModBlocks.PRISMARINE_GLOW_CAMPFIRE.get(),
-            new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS))
+    public static final RegistryObject<Item> PRISMARINE_GLOW_CAMPFIRE = ITEMS.register(
+        "prismarine_glow_campfire",
+        () -> new ItemNameBlockItem(ModBlocks.PRISMARINE_GLOW_CAMPFIRE.get(),
+        new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS))
     );
 
-    public static final RegistryObject<Item> PRISMARINE_BRICK_GLOW_CAMPFIRE = ITEMS.register("prismarine_brick_glow_campfire",
-            () -> new ItemNameBlockItem(ModBlocks.PRISMARINE_BRICK_GLOW_CAMPFIRE.get(),
-                    new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS))
+    public static final RegistryObject<Item> PRISMARINE_BRICK_GLOW_CAMPFIRE = ITEMS.register(
+        "prismarine_brick_glow_campfire",
+        () -> new ItemNameBlockItem(ModBlocks.PRISMARINE_BRICK_GLOW_CAMPFIRE.get(), new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS))
     );
 
-    public static final RegistryObject<Item> DARK_PRISMARINE_GLOW_CAMPFIRE = ITEMS.register("dark_prismarine_glow_campfire",
-            () -> new ItemNameBlockItem(ModBlocks.DARK_PRISMARINE_GLOW_CAMPFIRE.get(),
-                    new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS))
+    public static final RegistryObject<Item> DARK_PRISMARINE_GLOW_CAMPFIRE = ITEMS.register(
+        "dark_prismarine_glow_campfire",
+        () -> new ItemNameBlockItem(ModBlocks.DARK_PRISMARINE_GLOW_CAMPFIRE.get(),
+        new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS))
     );
 
     public static void register(IEventBus eventBus){
