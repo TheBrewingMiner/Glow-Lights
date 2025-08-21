10222f7c98017ff261920af2c119a7eaa11d59f9
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-08-20 17:32:37 -0700

    Fixed duplicate mapping which was causing a crash upon use of axe.

--- src/main/java/net/thebrewingminer/glowlights/block/copper/utils/WeatheringBlockMap.java
+++ src/main/java/net/thebrewingminer/glowlights/block/copper/utils/WeatheringBlockMap.java
@@ -34,7 +34,7 @@ public class WeatheringBlockMap {
                 .put(ModBlocks.OXIDIZED_COPPER_GLOW_TORCH.get(), ModBlocks.WAXED_OXIDIZED_COPPER_GLOW_TORCH.get())
                 .put(ModBlocks.OXIDIZED_COPPER_GLOW_WALL_TORCH.get(), ModBlocks.WAXED_OXIDIZED_COPPER_GLOW_WALL_TORCH.get())
                 .put(ModBlocks.COPPER_GLOW_LANTERN.get(), ModBlocks.WAXED_COPPER_GLOW_LANTERN.get())
-                .put(ModBlocks.WEATHERED_COPPER_GLOW_LANTERN.get(), ModBlocks.WAXED_WEATHERED_COPPER_GLOW_LANTERN.get())
+                .put(ModBlocks.EXPOSED_COPPER_GLOW_LANTERN.get(), ModBlocks.WAXED_EXPOSED_COPPER_GLOW_LANTERN.get())
                 .put(ModBlocks.WEATHERED_COPPER_GLOW_LANTERN.get(), ModBlocks.WAXED_WEATHERED_COPPER_GLOW_LANTERN.get())
                 .put(ModBlocks.OXIDIZED_COPPER_GLOW_LANTERN.get(), ModBlocks.WAXED_OXIDIZED_COPPER_GLOW_LANTERN.get())
             .build();
