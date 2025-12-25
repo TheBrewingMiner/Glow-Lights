b4541e594a2d947655d0a5092757caee500e08dd
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-12-24 16:49:12 -0800

    Fixed copper campfires not lighting from dispensers, as well as fixed lighting behavior to be consistent with GlowCampfireBlocks. Dispensers ignore has_ash_unlit state.

--- src/main/java/net/thebrewingminer/glowlights/mixin/FireChargeItemMixin.java
+++ src/main/java/net/thebrewingminer/glowlights/mixin/FireChargeItemMixin.java
@@ -5,8 +5,10 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.FireChargeItem;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.thebrewingminer.glowlights.block.GlowCampfireBlock;
+import net.thebrewingminer.glowlights.block.copper.utils.ICopperCampfireVariant;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,7 +26,8 @@ public class FireChargeItemMixin {
         Level level = pContext.getLevel();
         BlockPos pos = pContext.getClickedPos();
         BlockState state = level.getBlockState(pos);
+        Block block = state.getBlock();
 
-        if (state.getBlock() instanceof GlowCampfireBlock) cir.setReturnValue(InteractionResult.FAIL);
+        if (block instanceof GlowCampfireBlock || block instanceof ICopperCampfireVariant) cir.setReturnValue(InteractionResult.FAIL);
     }
 }
