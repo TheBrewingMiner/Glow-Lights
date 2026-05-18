de377a42a80ba39782eda583bfadd0dcbf155312
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2026-05-17 18:45:07 -0700

    Adapted events to handle the logic themselves with the 1.20.3 logical bug.

--- src/main/java/net/thebrewingminer/glowlights/event/ScrapeEvent.java
+++ src/main/java/net/thebrewingminer/glowlights/event/ScrapeEvent.java
@@ -1,5 +1,7 @@
 package net.thebrewingminer.glowlights.event;
 
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
@@ -14,12 +16,10 @@ import net.thebrewingminer.glowlights.block.copper.utils.IWeatheringCopper;
 
 @Mod.EventBusSubscriber(modid = GlowLights.MOD_ID)
 public class ScrapeEvent {
-    // Disabled both from event registration to handle custom logic in the blocks themselves.
     // It seems that in Forge 1.20.3, the event is broadcasted for axes ONLY when the tool action is AXE_STRIP.
 
-
     // Handle scraping for custom copper when an axe is used on it.
-//    @SubscribeEvent
+    @SubscribeEvent
     public static void scrapeOxidation(BlockEvent.BlockToolModificationEvent toolEvent){
         UseOnContext useOnContext = toolEvent.getContext();
         ItemStack heldItem = useOnContext.getItemInHand();
@@ -29,17 +29,26 @@ public class ScrapeEvent {
 
         boolean isCopper = (block instanceof IWeatheringCopper);
         boolean usingAxe = heldItem.is(ItemTags.AXES);
-        boolean isScrapingOff = (toolEvent.getToolAction() == ToolActions.AXE_SCRAPE);
+        boolean isScrapingOff = (toolEvent.getToolAction() == ToolActions.AXE_STRIP);
 
         if (!isCopper) return;
         if (!usingAxe) return;
         if (!isScrapingOff) return;
 
-        IWeatheringCopper.getPrevious(block).ifPresent(scraped -> toolEvent.setFinalState(scraped.withPropertiesOf(blockState)));
+        IWeatheringCopper.getPrevious(block).ifPresent(scraped -> {
+            toolEvent.setFinalState(scraped.withPropertiesOf(blockState));
+            if (!toolEvent.isSimulated()) {
+                useOnContext.getLevel().playSound(
+                        useOnContext.getPlayer(), useOnContext.getClickedPos(),
+                        SoundEvents.AXE_SCRAPE, SoundSource.BLOCKS, 1.0F, 1.0F
+                );
+                useOnContext.getLevel().levelEvent(useOnContext.getPlayer(), 3005, useOnContext.getClickedPos(), 0);
+            }
+        });
     }
 
     // Handle scraping wax for custom waxed copper when an axe is used on it.
-//    @SubscribeEvent
+    @SubscribeEvent
     public static void scrapeWax(BlockEvent.BlockToolModificationEvent toolEvent){
         UseOnContext useOnContext = toolEvent.getContext();
         ItemStack heldItem = useOnContext.getItemInHand();
@@ -48,11 +57,17 @@ public class ScrapeEvent {
         Block block = blockState.getBlock();
 
         boolean usingAxe = heldItem.is(ItemTags.AXES);
-        boolean isWaxingOff = (toolEvent.getToolAction() == ToolActions.AXE_WAX_OFF);
+        boolean isWaxingOff = (toolEvent.getToolAction() == ToolActions.AXE_STRIP);
 
         if (!usingAxe) return;
         if (!isWaxingOff) return;
 
-        IWeatheringCopper.getUnwaxed(block).ifPresent(waxed -> toolEvent.setFinalState(waxed.withPropertiesOf(blockState)));
+        IWeatheringCopper.getUnwaxed(block).ifPresent(scraped -> {
+            toolEvent.setFinalState(scraped.withPropertiesOf(blockState));
+            if (!toolEvent.isSimulated()) {
+                useOnContext.getLevel().playSound(useOnContext.getPlayer(), useOnContext.getClickedPos(), SoundEvents.AXE_WAX_OFF, SoundSource.BLOCKS, 1.0F, 1.0F);
+                useOnContext.getLevel().levelEvent(useOnContext.getPlayer(), 3004, useOnContext.getClickedPos(), 0);
+            }
+        });
     }
 }
