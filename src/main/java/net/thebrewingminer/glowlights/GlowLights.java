f5c4151955d2c95f7061d9fba63c74a5e4349d55
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2026-06-27 21:24:39 -0700

    Fixed mod constructor.

--- src/main/java/net/thebrewingminer/glowlights/GlowLights.java
+++ src/main/java/net/thebrewingminer/glowlights/GlowLights.java
@@ -19,8 +19,8 @@ import net.thebrewingminer.glowlights.init.ModParticles;
 public class GlowLights {
     public static final String MOD_ID = "glowlights";
 
-    public GlowLights(FMLJavaModLoadingContext modLoadingContext) {
-        IEventBus modEventBus = modLoadingContext.getModEventBus();
+    public GlowLights() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register all registries.
         ModItems.register(modEventBus);
